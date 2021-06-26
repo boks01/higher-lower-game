8d151e9d3cedef5720512bda836fe5639e12ac81
Boks1 <msal40755@gmail.com>
2021-06-26 16:59:08 +0700

    membuat window baru di main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+from tkinter import *
+import pandas as pd
+
+bg = "#CDF0EA"
+
+# ==================working area===============
+window = Tk()
+window.title("higher lower")
+window.minsize(width=300, height=400)
+window.config(padx=30, pady=30, bg=bg)
+window.mainloop()
