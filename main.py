83c7cb90dfb882dcb2cf07604d39b1293fd2f6ff
Boks1 <msal40755@gmail.com>
2021-06-27 09:35:58 +0700

    membuat function baru dan menampilkannya

--- main.py
+++ main.py
@@ -12,10 +12,23 @@ name2 = ""
 folower_count1 = ""
 folower_count2  = ""
 #===================function=================
-def compare():
+def no_compare():
     global folower_count1, folower_count2
     first_compare.config(text=f"{folower_count1}M")
     second_compare.config(text=f"{folower_count2}M")
+    if folower_count1 < folower_count2:
+        asking.config(text="You right!!")
+    else:
+        asking.config(text="Incorrect answer\nyou lose")
+
+def yes_compare():
+    global folower_count1, folower_count2
+    first_compare.config(text=f"{folower_count1}M")
+    second_compare.config(text=f"{folower_count2}M")
+    if folower_count1 > folower_count2 or folower_count1 == folower_count2:
+        asking.config(text="You right!!")
+    else:
+        asking.config(text="Incorrect answer\nyou lose")
 def random_compare():
     global name1, name2, folower_count1, folower_count2 
     first_compare.config(text=name1)
@@ -31,7 +44,6 @@ def random_compare():
 # ==================working area===============
 window = Tk()
 window.title("higher lower")
-window.minsize(width=800, height=400)
 window.config(padx=100, pady=80, bg=BG)
 # ========== all text ==============
 A = Label(text="A", font=("ariel", 25, "italic"), bg=BG)
@@ -39,7 +51,8 @@ A.grid(row=0, column=0)
 first_compare = D=Label(text="First\nCompare", font=("ariel", 35, "bold"), bg=BG)
 first_compare.grid(row=1, column=0)
 
-score = Label(text=SCORE, font=("ariel", 35, "bold"), bg=BG)
+score = Label(text=SCORE, font=("ariel", 35), bg=BG)
+score.grid(row=0, column=1)
 
 vs = Label(text="VS", font=("ariel", 35, "italic"), bg=BG)
 vs.grid(row=1, column=1)
@@ -57,13 +70,14 @@ asking.grid(row=2, column=1)
 # asking.config(pady=40)
 #============== images =================
 yes = PhotoImage(file="images/right.png")
-yes_button = Button(image=yes)
+yes_button = Button(image=yes, command=yes_compare)
 yes_button.grid(row=3, column=2)
 yes_button.config(highlightthickness=0)
 
 no = PhotoImage(file="images/wrong.png")
-no_button = Button(image=no)
+no_button = Button(image=no, command=no_compare)
 no_button.grid(row=3, column=0)
 no_button.config(highlightthickness=0)
+
 random_compare()
 window.mainloop()
